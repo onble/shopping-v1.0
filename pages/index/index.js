edc8384d4f3223b7cdbb60a68aac6df346945619
“onble” <62949816+onble@users.noreply.github.com>
2023-06-19 17:42:19 +0800

    完成第一次文档的所有内容

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 // index.js
 // 导入request请求工具类
-import {requestUtil} from '../../utils/requestUtil.js';
+import {getBaseUrl,requestUtil} from '../../utils/requestUtil.js';
 Page({
 
   /**
@@ -8,16 +8,22 @@ Page({
    */
   data: {
     // 轮播图数组
-    swiperList:[]
+    swiperList:[],
+    baseUrl:'',// 定义一个请求路径变量，初始化为空
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    requestUtil({url: '/product/findSwiper',method:"GET"}).then(result=>{
+    requestUtil({url: '/product/findSwiper' // 地址在工具里已经把项目地址拼接好了
+                ,method:"GET"}).then(result=>{
+        // 2 页面加载的时候，获取baseUrl项目路径并赋值
+        const baseUrl = getBaseUrl();
         this.setData({
-             swiperList:result.message
+             swiperList:result.message,
+             baseUrl,
+             // baseUrl名字一样客户以省略，就是利用ES6拆包时候覆盖变量
         })
     })
 }
