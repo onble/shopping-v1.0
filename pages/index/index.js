e6789322c42ebce4575117a8f00b85684ad9a2b8
“onble” <62949816+onble@users.noreply.github.com>
2023-06-19 17:12:24 +0800

    实现动态加载轮播图信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,10 +15,11 @@ Page({
   onLoad: function (options) {
     // 发送异步请求，获取后端数据
     wx.request({
+      // 下面的请求无法发出，参考https://blog.csdn.net/XiaoC_ong/article/details/104364448
       url: 'http://localhost:8080/product/findSwiper',
       method:"GET",
       success:(result)=>{
-        console.log(result)
+        console.log('res',result)
         this.setData({
           swiperList:result.data.message
         })
